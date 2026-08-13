25591ec0fd5268c27918c36ed1a4e54e4681f0c3
Harsh Singh Chauhan <hsking.hh@gmail.com>
2026-08-13 15:22:20 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+
+private:
+    void func(int open,int close,int n,string s,vector<string>& ans){
+        if(open+close==2*n && open==close){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            func(open+1,close,n,s+'(',ans);
+        }
+        if(close<open){
+            func(open,close+1,n,s+')',ans);
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        func(0,0,n,"",ans);
+
+        return ans;
+    }
+};
